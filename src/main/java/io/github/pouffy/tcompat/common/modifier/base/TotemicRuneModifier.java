950950b540a550b3b3dade578e08599da6f3efe2
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-05-24 23:34:20 +0100

    de-abstract TotemicRuneModifier

--- src/main/java/io/github/pouffy/tcompat/common/modifier/base/TotemicRuneModifier.java
+++ src/main/java/io/github/pouffy/tcompat/common/modifier/base/TotemicRuneModifier.java
@@ -1,6 +1,8 @@
 package io.github.pouffy.tcompat.common.modifier.base;
 
 import io.github.pouffy.tcompat.common.modifier.hook.CurioTickModifierHook;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -9,13 +11,30 @@ import slimeknights.tconstruct.library.modifiers.hook.interaction.InventoryTickM
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
-public abstract class TotemicRuneModifier extends NoLevelsModifier implements CurioTickModifierHook, InventoryTickModifierHook {
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class TotemicRuneModifier extends NoLevelsModifier implements CurioTickModifierHook, InventoryTickModifierHook {
+    public final Supplier<MobEffect> mobEffectSupplier;
+    public final Predicate<LivingEntity> entityPredicate;
+    private final int interval;
+
+    protected TotemicRuneModifier(Supplier<MobEffect> mobEffectSupplier, Predicate<LivingEntity> entityPredicate, int interval) {
+        this.mobEffectSupplier = mobEffectSupplier;
+        this.entityPredicate = entityPredicate;
+        this.interval = interval;
+    }
 
     public void curioTick(IToolStackView tool, ModifierEntry entry, String slotId, int slotIndex, LivingEntity wearer, ItemStack stack) {
-
+        if (!wearer.level().isClientSide() && wearer.level().getGameTime() % (long)this.interval == 0L && this.entityPredicate.test(wearer)) {
+            wearer.addEffect(new MobEffectInstance(this.mobEffectSupplier.get(), 200, 0, true, true));
+        }
     }
 
     public void onInventoryTick(IToolStackView tool, ModifierEntry modifier, Level world, LivingEntity living, int itemSlot, boolean isSelected, boolean isCorrectSlot, ItemStack stack) {
-
+        if (!isCorrectSlot || tool.isBroken()) return;
+        if (!living.level().isClientSide() && living.level().getGameTime() % (long)this.interval == 0L && this.entityPredicate.test(living)) {
+            living.addEffect(new MobEffectInstance(this.mobEffectSupplier.get(), 200, 0, true, true));
+        }
     }
 }
