8f2e0409b4d8f803d45be485871e7eb9ba67de5f
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-07-28 16:48:16 +0100

    Copper appliance melting

--- src/main/java/io/github/pouffy/tcompat/compat/create/CreateSmeltery.java
+++ src/main/java/io/github/pouffy/tcompat/compat/create/CreateSmeltery.java
@@ -18,11 +18,13 @@ public class CreateSmeltery implements CompatSmeltery {
         simpleMelting(cConsumer, TinkerFluids.moltenGold, FluidValues.INGOT * 2, "gold", Ingredient.of(TCTags.Items.named("create", "toolboxes")), metalFolder("melting"), "toolbox");
         simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT * 3, "copper", Ingredient.of(TCTags.Items.named("create", "valve_handles")), metalFolder("melting"), "valve_handle");
 
-        meltEquipment(cConsumer);
-
+        equipment(cConsumer);
+        kinetics(cConsumer);
+        fluids(cConsumer);
+        decorations(cConsumer);
     }
 
-    private void meltEquipment(Consumer<FinishedRecipe> cConsumer) {
+    private void equipment(Consumer<FinishedRecipe> cConsumer) {
         simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT * 12, "copper", listedInput("copper_backtank"), metalFolder("melting"), "backtank", new int[]{FluidValues.NUGGET});
         simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT * 4, "copper", listedInput("copper_diving_boots"), metalFolder("melting"), "diving_boots", new int[]{FluidValues.NUGGET});
         simpleMelting(TinkerFluids.moltenCopper, FluidValues.INGOT * 5, listedInput("copper_diving_helmet"))
@@ -72,6 +74,37 @@ public class CreateSmeltery implements CompatSmeltery {
                 .save(cConsumer, location(metalFolder("melting") + "/gold/goggles"));
 
         simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT * 3, "copper", listedInput("linked_controller"), metalFolder("melting"), "linked_controller");
+    }
+
+    private void kinetics(Consumer<FinishedRecipe> cConsumer) {
+
+    }
+
+    private void fluids(Consumer<FinishedRecipe> cConsumer) {
+        simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT, "copper", listedInput("copper_casing"), metalFolder("melting"), "casing");
+        simpleMelting(cConsumer, TinkerFluids.moltenCopper, 67, "copper", listedInput("fluid_pipe", "mechanical_pump"), metalFolder("melting"), "fluid_pipes");
+        simpleMelting(TinkerFluids.moltenBrass, FluidValues.INGOT, listedInput("smart_fluid_pipe"))
+                .addByproduct(TinkerFluids.moltenIron.result(FluidValues.INGOT))
+                .addByproduct(TinkerFluids.moltenCopper.result(67))
+                .save(cConsumer, location(metalFolder("melting") + "/brass/smart_fluid_pipe"));
+        simpleMelting(TinkerFluids.moltenIron, FluidValues.INGOT, listedInput("fluid_valve"))
+                .addByproduct(TinkerFluids.moltenCopper.result(67))
+                .save(cConsumer, location(metalFolder("melting") + "/iron/fluid_valve"));
+        simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT * 2, "copper", listedInput("fluid_tank", "hose_pulley"), metalFolder("melting"), "fluid_tank");
+        simpleMelting(TinkerFluids.moltenCopper, FluidValues.INGOT, listedInput("item_drain"))
+                .addByproduct(TinkerFluids.moltenIron.result(FluidValues.NUGGET * 3))
+                .save(cConsumer, location(metalFolder("melting") + "/copper/item_drain"));
+        simpleMelting(cConsumer, TinkerFluids.moltenCopper, FluidValues.INGOT, "copper", listedInput("spout"), metalFolder("melting"), "spout");
+        simpleMelting(TinkerFluids.moltenCopper, FluidValues.INGOT, listedInput("steam_engine", "steam_whistle"))
+                .addByproduct(TinkerFluids.moltenGold.result(FluidValues.INGOT))
+                .save(cConsumer, location(metalFolder("melting") + "/copper/steam_appliances"));
+        simpleMelting(TinkerFluids.moltenCopper, FluidValues.INGOT, listedInput("portable_fluid_interface"))
+                .addByproduct(TinkerFluids.moltenIron.result(67))
+                .save(cConsumer, location(metalFolder("melting") + "/copper/portable_fluid_interface"));
+    }
+
+    private void decorations(Consumer<FinishedRecipe> cConsumer) {
+
     }
 
     @Override
